17695c92f77348121c24c47d05db229ab0d48cb8
Patrick Naughton <patricknaughton01@gmail.com>
2019-02-20 09:49:31 -0600

    WIP wiring up usb

--- RichArduino/testbench.vhd
+++ RichArduino/testbench.vhd
@@ -11,11 +11,16 @@ USE IEEE.STD_LOGIC_ARITH.ALL ;
 ENTITY testbench IS
    PORT(clk       : IN  STD_LOGIC;
         reset_l   : IN  STD_LOGIC;
+		  txe_l		: IN  STD_LOGIC;
+		  rxf_l		: IN	STD_LOGIC;
         r         : OUT STD_LOGIC_VECTOR(1 DOWNTO 0) ;
         g         : OUT STD_LOGIC_VECTOR(1 DOWNTO 0) ;
         b         : OUT STD_LOGIC_VECTOR(1 DOWNTO 0) ;
         hs        : OUT STD_LOGIC ;
-        vs        : OUT STD_LOGIC);
+        vs        : OUT STD_LOGIC ;
+		  usb_bus	: OUT	STD_LOGIC_VECTOR(7 DOWNTO 0) ;
+		  usb_rd		: OUT	STD_LOGIC ;
+		  usb_wr		: OUT STD_LOGIC);
 END testbench ;
 
 ARCHITECTURE structure OF testbench IS
@@ -62,7 +67,22 @@ ARCHITECTURE structure OF testbench IS
 --        b        : OUT STD_LOGIC_VECTOR(1 DOWNTO 0) ;
 --        hs       : OUT STD_LOGIC ;
 --        vs       : OUT STD_LOGIC);
---   END COMPONENT ;           
+--   END COMPONENT ;   
+		
+	COMPONENT usb
+	PORT (  d_bus		:	INOUT	STD_LOGIC_VECTOR(7 DOWNTO 0);
+			  d_usb		:	INOUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			  clk			:	IN		STD_LOGIC;
+			  usb_rd_h	:	IN		STD_LOGIC;
+			  usb_wr_h	:	IN		STD_LOGIC;
+			  txe_oe_l	:	IN		STD_LOGIC;
+			  rxf_oe_l	:	IN		STD_LOGIC;
+			  txe_l		:	IN		STD_LOGIC;
+			  rxf_l		:	IN		STD_LOGIC;
+			  rd_l		:	OUT	STD_LOGIC;
+			  wr_h		:	OUT	STD_LOGIC;
+			  usb_done	:	INOUT	STD_LOGIC);
+	END COMPONENT;
 
    SIGNAL reset_l_temp : STD_LOGIC ;
    SIGNAL reset_l_sync : STD_LOGIC ;
@@ -79,6 +99,11 @@ ARCHITECTURE structure OF testbench IS
    SIGNAL sram_we_l    :  STD_LOGIC ;
    SIGNAL vga_ena      : STD_LOGIC;
    SIGNAL vga_wea      : STD_LOGIC_VECTOR(0 DOWNTO 0) ;
+	SIGNAL usb_done	  : STD_LOGIC ;
+	SIGNAL usb_oe_h	  : STD_LOGIC ;
+	SIGNAL usb_we_h	  : STD_LOGIC ;
+	SIGNAL usb_txe_oe_l : STD_LOGIC ;
+	SIGNAL usb_rxf_oe_l : STD_LOGIC ;
 
 BEGIN
 
@@ -103,7 +128,7 @@ BEGIN
    sram_we_l  <= '0' WHEN write = '1' ELSE '1' ;
 
 --   done <= '1' WHEN (eprom_ce_l = '0' OR sram_ce_l = '0' OR vga_ena = '1') ELSE '0' ;
-   done <= '1' WHEN (eprom_ce_l = '0' OR sram_ce_l = '0') ELSE '0' ;
+   done <= '1' WHEN (eprom_ce_l = '0' OR sram_ce_l = '0' OR usb_done = '1') ELSE '0' ;
 	
    rsrc1:rsrc      
    PORT MAP(clk       => clk_out,
@@ -142,5 +167,18 @@ BEGIN
 --            b         => b,
 --            hs        => hs,
 --            vs        => vs);
+		usb1:usb
+		PORT MAP(  d_bus		=> d(7 DOWNTO 0),
+					  d_usb		=> usb_bus,
+					  clk			=> clk_out,
+					  usb_rd_h	=> usb_oe_h,
+					  usb_wr_h	=> usb_we_h,
+					  txe_oe_l	=> usb_txe_oe_l,
+					  rxf_oe_l	=> usb_rxf_oe_l,
+					  txe_l		=> txe_l,
+					  rxf_l		=> rxf_l,
+					  rd_l		=> usb_rd,
+					  wr_h		=> usb_wr,
+					  usb_done	=> usb_done);
 
 END structure;
